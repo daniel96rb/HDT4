fafadda94cd34f066ff1959135fb8b939db8a443
daniel96rb <drodriguezbianchi96@gmail.com>
2017-02-22 13:38:17 -0600

    Create iPila.java

---
+++ iPila.java
@@ -0,0 +1,13 @@
+/* Interface iCalculadora que contiene los metodos para la calculadora */
+public interface iPila<E>{
+
+	/* Determina si la pila esta vacia */
+	public boolean isEmpty();
+
+	/* Realiza el push del operando */
+	public void push(E digit);
+
+	/* Realiza el pop del resultado */
+	public E pop();
+
+}
