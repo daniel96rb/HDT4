d89f54c2152abb47eb5c4429c853b7062fccd1c4
daniel96rb <drodriguezbianchi96@gmail.com>
2017-02-22 23:00:09 -0600

    Create AbstractPila.java

---
+++ AbstractPila.java
@@ -0,0 +1,28 @@
+
+public abstract class AbstractPila implements iPila{
+
+    /** @param e */
+    @Override
+    public void push(Object digit){
+		    }
+
+    /** @return lastObjectInStack */
+    @Override
+    public Object pop(){
+        return null;
+    }
+
+    /**  @return vectorStatus */
+    @Override
+    public boolean isEmpty(){
+        return list.size() == 0;
+    }
+
+    /** @return vectorSize */
+    @Override
+    public int size(){
+
+        return 0;
+    }
+
+}
